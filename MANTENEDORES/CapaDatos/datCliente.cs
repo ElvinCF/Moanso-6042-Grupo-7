1781867a87967f880b70ddd8326f25ebfb1a881a
Alexander Garcia <alexandergarciasanchez9@gmail.com>
2023-10-25 04:23:35 -0500

    Revert "conexion"
    This reverts commit d42adee15f0178577ba10f14ee6f0065c34b7eaa.

--- MANTENEDORES/CapaDatos/datCliente.cs
+++ MANTENEDORES/CapaDatos/datCliente.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace CapaDatos
 {
-    internal class Conexion
+    public class datCliente
     {
-        public SqlConnection Conectar()
-        {
-            
-        }
     }
 }
