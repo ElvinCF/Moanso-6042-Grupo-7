bc6dce900c6c782e976618b8fe6e1834ec59cf03
Alexander Garcia <alexandergarciasanchez9@gmail.com>
2023-10-25 01:49:24 -0500

    cambio de conexion

--- MANTENEDORES/CapaDatos/Conexion.cs
+++ MANTENEDORES/CapaDatos/Conexion.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace CapaDatos
 {
     internal class Conexion
     {
+        public SqlConnection Conectar()
+        {
+            
+        }
     }
 }
