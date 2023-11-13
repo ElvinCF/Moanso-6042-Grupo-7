962405f7cb2a8d20c3a1e1ff88bf72af740809a4
Yeri Erwin Alegria Rodriguez <131008035+ErwinALegria@users.noreply.github.com>
2023-11-13 17:14:04 -0500

    venta

--- MANTENEDORES/Minimarket/MenuPrincipal.Designer.cs
+++ MANTENEDORES/Minimarket/MenuPrincipal.Designer.cs
@@ -146,7 +146,7 @@
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(143, 62);
             this.button8.TabIndex = 9;
-            this.button8.Text = "CategoriaProducto (CRUD REALIZADO)";
+            this.button8.Text = "Nueva categoria de Producto (CRUD REALIZADO)";
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
