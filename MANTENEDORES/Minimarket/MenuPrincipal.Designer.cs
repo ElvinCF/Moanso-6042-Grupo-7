164ab03fadf63f393b042d43c3114906e13413bc
Elvin Córdova <elvin.cf@outlook.com>
2023-11-21 02:21:09 -0500

    Acomodé el menu principal

--- MANTENEDORES/Minimarket/MenuPrincipal.Designer.cs
+++ MANTENEDORES/Minimarket/MenuPrincipal.Designer.cs
@@ -48,7 +48,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(59, 256);
+            this.button1.Location = new System.Drawing.Point(59, 221);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(6, 6);
@@ -59,7 +59,7 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.Window;
-            this.button2.Location = new System.Drawing.Point(16, 256);
+            this.button2.Location = new System.Drawing.Point(558, 269);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(117, 50);
@@ -70,7 +70,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(153, 256);
+            this.button3.Location = new System.Drawing.Point(18, 269);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(107, 50);
@@ -81,7 +81,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(286, 256);
+            this.button4.Location = new System.Drawing.Point(18, 338);
             this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(107, 50);
@@ -92,7 +92,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(419, 256);
+            this.button5.Location = new System.Drawing.Point(147, 269);
             this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(107, 50);
@@ -108,7 +108,7 @@
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label5.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label5.Location = new System.Drawing.Point(429, 7);
+            this.label5.Location = new System.Drawing.Point(465, 9);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(135, 24);
             this.label5.TabIndex = 2;
@@ -118,11 +118,11 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.textBox1.Location = new System.Drawing.Point(314, 36);
+            this.textBox1.Location = new System.Drawing.Point(403, 43);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(356, 174);
+            this.textBox1.Size = new System.Drawing.Size(272, 184);
             this.textBox1.TabIndex = 3;
             this.textBox1.Text = "Bienvenido a nuestro prototipo de software de Sistema de Ventas\r\n\r\nEstamos desarr" +
     "ollando un software integral de gestión para Negocios y Servicios La Plaza S.R.L" +
@@ -130,7 +130,7 @@
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(499, 332);
+            this.button8.Location = new System.Drawing.Point(147, 338);
             this.button8.Margin = new System.Windows.Forms.Padding(2);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(107, 50);
@@ -142,7 +142,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 224);
+            this.label1.Location = new System.Drawing.Point(15, 241);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(133, 13);
@@ -151,10 +151,10 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(552, 256);
+            this.button6.Location = new System.Drawing.Point(558, 338);
             this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(107, 50);
+            this.button6.Size = new System.Drawing.Size(117, 50);
             this.button6.TabIndex = 11;
             this.button6.Text = "Clientes";
             this.button6.UseVisualStyleBackColor = true;
@@ -162,7 +162,7 @@
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(213, 332);
+            this.button9.Location = new System.Drawing.Point(291, 338);
             this.button9.Margin = new System.Windows.Forms.Padding(2);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(107, 50);
@@ -173,7 +173,7 @@
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(70, 332);
+            this.button10.Location = new System.Drawing.Point(414, 338);
             this.button10.Margin = new System.Windows.Forms.Padding(2);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(107, 50);
@@ -184,7 +184,7 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(368, 332);
+            this.button7.Location = new System.Drawing.Point(353, 269);
             this.button7.Margin = new System.Windows.Forms.Padding(2);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(96, 50);
@@ -199,7 +199,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(9, 12);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(403, 245);
+            this.pictureBox1.Size = new System.Drawing.Size(375, 215);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -209,7 +209,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MintCream;
-            this.ClientSize = new System.Drawing.Size(678, 400);
+            this.ClientSize = new System.Drawing.Size(693, 408);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
